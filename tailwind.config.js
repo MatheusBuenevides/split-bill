c41a263753544eefff9d0eeaac80b11dfe1c010c
Matheus Buenevides <matheusgomesbuenevides@gmail.com>
2024-09-07 20:30:30 -0300

    Add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightGray: '#F5F5F5',
+        primary: '#48CFCB',
+        secondary: '#229799',
+        darkGray: '#424242',
+      },
+    },
   },
   plugins: [],
 }
